b525cafad4ccaafa887bcaa5df3697b3957ac3c5
zhuwenjun <zwj678900@163.com>
2024-06-12 10:09:23 +0800

    初始化

---
+++ blog-admin/src/settings.ts
@@ -0,0 +1,25 @@
+import { SizeEnum } from "./enums/SizeEnum";
+import { LayoutEnum } from "./enums/LayoutEnum";
+import { ThemeEnum } from "./enums/ThemeEnum";
+import { LanguageEnum } from "./enums/LanguageEnum";
+
+const defaultSettings: AppSettings = {
+  // title: import.meta.env.VITE_APP_TITLE,
+  // version: import.meta.env.VITE_APP_VERSION,
+  title: "流水博客后台系统",
+  version: "1.0",
+  showSettings: true,
+  tagsView: true,
+  fixedHeader: true,
+  sidebarLogo: true,
+  layout: LayoutEnum.LEFT,
+  theme: ThemeEnum.LIGHT,
+  size: SizeEnum.DEFAULT,
+  language: LanguageEnum.ZH_CN,
+  themeColor: "#409EFF",
+  watermarkEnabled: false,
+  watermarkContent: import.meta.env.VITE_APP_TITLE,
+  logo: import.meta.env.VITE_APP_LOGO,
+};
+
+export default defaultSettings;
